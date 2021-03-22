39f590aa79d4fe8403eecced0a19f85551304eaa
aceforeverd <teapot@aceforeverd.com>
2021-03-22 17:01:34 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # github-action-playground
 Play around Github actions
+
+# linters
++ cpplint
++ shellcheck
++ languagetool
++ markdownlint
++ detect-secrets
--- src/example.cc
+++ src/example.cc
@@ -2,6 +2,8 @@
 
 int main()
 {
+
+    // this is a line that should be more than 80 characters, water water water water water babababa
     example::PlayGround pg(0, "lol");
     pg.play();
     return 0;
