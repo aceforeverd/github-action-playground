827413e7e559de24ccaf102db8c2b73075cf54c6
aceforeverd <teapot@aceforeverd.com>
2021-03-22 19:58:56 +0800

    update flake8 lint

--- src/example.py
+++ src/example.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 class HelloWorld(object):
-
-    """Docstring for PlayGround. """
-
     def __init__(self, id, name):
-        """TODO: to be defined.
-
-        :Docstring for PlayGround.: TODO
-
-        """
-
         self.idx = id
         self.namex = name
-
     def play(self):
         print('idx: {}, namex: {}', self.idx, self.namex)
-
-
 if __name__ == '__main__':
     HelloWorld(1, '12')
     print('hello world')
